01e479f953d92d1f18b57df3973e65c86fe44138
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2025-04-07 10:55:11 +0530

    First-commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: '--font-body',
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className={`${manrope.variable} font-body text-xl`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
