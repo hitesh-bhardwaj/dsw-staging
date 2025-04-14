dab4cadf42ff07a1d1db948fcb72bd2f7cfa72b3
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-04-14 19:05:03 +0530

    Update Clients.jsx

--- src/components/Homepage/Clients.jsx
+++ src/components/Homepage/Clients.jsx
@@ -15,91 +15,65 @@ const Clients = () => {
         scrollTrigger: {
           trigger: sectionRef.current,
           start: 'top top',
-          end: '+200% bottom',
-          scrub: 0.8,
+          end: '+30% top',
+          scrub: true,
           pin:true,
           // markers:true
         },
       });
-      // tlZ.fromTo(
-      //   imageRefs.current,
-      //   {
-      //     transform: 'translateZ(-2000px)',
-      //     delay: -0.8,
-      //     stagger: { each: 0.05, from: 'random' },
-      //   },
-      //   {
-      //     transform: 'translateZ(1000px)',
-      //     scale:0.4,
-      //     stagger: { each: 0.05, from: 'random' },
-      //     ease: 'none',
-      //   }
-      // );
+
+   const tmove = gsap.timeline({
+    scrollTrigger:{
+      trigger:sectionRef.current,
+      // markers:true,
+      start:"top bottom",
+      end:"+150% top",
+      scrub:true
+    }
+   })
+      tmove.fromTo(
+        imageRefs.current,
+        {
+          transform: 'translateZ(-1000px)',
+          delay: -0.8,
+         
+        },
+        {
+          transform: 'translateZ(500px)',
+          // scale:0.4,
+          stagger:0.03,
+          ease: 'none',
+        }
+      );
       const tlBlur = gsap.timeline({
         scrollTrigger: {
           trigger: sectionRef.current,
-          start: 'top bottom',
-          end: '+100% bottom',
+          start: 'top 70%',
+          end: '+90% top',
           scrub: true,
-          // markers:true
+          // markers:true,
+       
         },
       });
   
-      tlBlur.fromTo(
+      tlBlur.from(
         imageRefs.current,
         {
-          transform: 'translateZ(-2000px)',
-          delay: -0.8,
-          // stagger: { each: 0.02, from: 'random' },
+          delay:-0.5,
+          stagger: { each: 0.1, from: 'random' },
           filter: 'blur(15px)',
           opacity:0,
-        },
-        {
-          transform: 'translateZ(-500px)',
-         
-          filter: 'blur(0px)',
-          opacity:1,
-          // stagger:0.2,
-          // stagger: { each: 0.04, from: 'random' },
-          ease: 'none',
         }
+        
       )
-      const tlblurC = gsap.timeline({
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          start: '100% bottom',
-          end: '+200% bottom',
-          scrub: true,
-          // markers:true
-        },
-      });
-      tlblurC.fromTo(
-        imageRefs.current,
-        {
-          transform: 'translateZ(-500px)',
-          delay:-0.5,
-       
-          filter: 'blur(0px)',
-        },
-        {
-          transform: 'translateZ(700px)',
-          scale:1,
-          // stagger:0.5,
-          filter: 'blur(15px)',
-          opacity:1,
-          ease: 'none',
-        }
-      )
-      // .to(
-      //   imageRefs.current,
-      //   { transform: 'translateZ(1000px)',
-      //     filter: 'blur(15px)',
-      //     delay:-0.4,
-      //     opacity:0,
-      //     stagger: { each: 0.05, from: 'random' },
-      //     ease: 'none',
-      //   }
-      // );
+      .to(imageRefs.current,{
+        // delay: -0.5,
+        stagger: { each: 0.1, from: 'random' },
+        filter: 'blur(15px)',
+        opacity:0,
+
+      })
+    
     }, sectionRef);
   
     return () => ctx.revert();
@@ -118,7 +92,7 @@ const Clients = () => {
       <div
         key={index}
         ref={el => imageRefs.current[index] = el}
-        className={`absolute ${img.style}`}
+        className={`absolute ${img.style} !scale-[0.7]`}
       >
         <Image
           quality={100}
@@ -145,49 +119,49 @@ const imageData = [
     alt: "manipal",
     height: 145,
     width: 276,
-    style: "top-5 left-10 blur-xs",
+    style: "top-5 left-10 " ,
   },
   {
     src: "/assets/images/edgeverve.png",
     alt: "edgeverve",
     height: 150,
     width: 290,
-    style: "top-2 left-[40vw] blur-xs",
+    style: "top-2 left-[40vw] ",
   },
   {
     src: "/assets/images/sodexo.png",
     alt: "sodexo",
     height: 89,
     width: 277,
-    style: "top-5 right-30",
+    style: "top-5 right-30 ",
   },
   {
     src: "/assets/images/canara.png",
     alt: "canara",
     height: 65,
     width: 218,
-    style: "bottom-0 -right-20 blur-xs",
+    style: "bottom-0 -right-20 ",
   },
   {
     src: "/assets/images/bon-prix.png",
     alt: "bon-prix",
     height: 102,
     width: 111,
-    style: "bottom-[-5vw] right-[43vw] blur-sm", 
+    style: "bottom-[-5vw] right-[43vw] ", 
   },
   {
     src: "/assets/images/ciek.png",
     alt: "ciek",
     height: 68,
     width: 216,
-    style: "bottom-4 left-20",
+    style: "bottom-4 left-20 ",
   },
   {
     src: "/assets/images/manipal.png",
     alt: "manipal",
     height: 145,
     width: 276,
-    style: "bottom-[-10vw] right-[15vw] blur-xs",
+    style: "bottom-[-10vw] right-[15vw]  ",
 
   },
   {
@@ -195,7 +169,7 @@ const imageData = [
     alt: "edgeverve",
     height: 150,
     width: 290,
-    style: "bottom-[-10vw] left-[25vw] blur-sm", 
+    style: "bottom-[-10vw] left-[25vw] ", 
 
   },
   {
@@ -203,7 +177,7 @@ const imageData = [
     alt: "sodexo",
     height: 89,
     width: 277,
-    style: "bottom-[10vw] left-[-20vw]",
+    style: "bottom-[10vw] left-[-20vw] ",
 
   },
   {
@@ -211,14 +185,14 @@ const imageData = [
     alt: "canara",
     height: 65,
     width: 218,
-    style: "top-[-10vw] left-[20vw] blur-xs",
+    style: "top-[-10vw] left-[20vw] ",
   },
   {
     src: "/assets/images/bon-prix.png",
     alt: "bon-prix",
     height: 102,
     width: 111,
-    style: "top-[-15vw] right-[30vw] blur-xs",
+    style: "top-[-15vw] right-[30vw] ",
 
   },
   {
@@ -226,7 +200,7 @@ const imageData = [
     alt: "ciek",
     height: 68,
     width: 216,
-    style: "bottom-[20vw] right-[-10vw]",
+    style: "bottom-[20vw] right-[-10vw] ",
 
   },
 ];
